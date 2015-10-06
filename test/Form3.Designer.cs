d20e0c2f182666c920f694e4702d5cddde428981
IvomirIvanov <sprelix@abv.bg>
2015-10-06 14:58:33 +0300

    added credits and starting menu background fixed

--- test/Form3.Designer.cs
+++ test/Form3.Designer.cs
@@ -32,15 +32,16 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.Transparent;
-            this.button1.Font = new System.Drawing.Font("Ravie", 30F);
-            this.button1.Location = new System.Drawing.Point(197, 140);
+            this.button1.Font = new System.Drawing.Font("Ravie", 20F);
+            this.button1.Location = new System.Drawing.Point(334, 162);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(284, 140);
+            this.button1.Size = new System.Drawing.Size(284, 88);
             this.button1.TabIndex = 0;
             this.button1.Text = "Start game";
             this.button1.UseVisualStyleBackColor = false;
@@ -49,10 +50,10 @@
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.Transparent;
-            this.button2.Font = new System.Drawing.Font("Ravie", 30F);
-            this.button2.Location = new System.Drawing.Point(197, 286);
+            this.button2.Font = new System.Drawing.Font("Ravie", 20F);
+            this.button2.Location = new System.Drawing.Point(334, 256);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(284, 138);
+            this.button2.Size = new System.Drawing.Size(284, 88);
             this.button2.TabIndex = 1;
             this.button2.Text = "Exit Game";
             this.button2.UseVisualStyleBackColor = false;
@@ -69,12 +70,25 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "Falling Rocks Plus";
             // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Ravie", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(334, 350);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(283, 88);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Credits";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(630, 468);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -91,5 +105,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
     }
 }
--- test/Form4.Designer.cs
+++ test/Form4.Designer.cs
@@ -28,11 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form4));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Ravie", 10F);
+            this.label1.Location = new System.Drawing.Point(133, 221);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Volen Aleksandrov";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(161, 240);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Daniela Valcheva";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(218, 259);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Alex Tinev";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Ravie", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(186, 278);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ivomir Ivanov";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Ravie", 12F);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(104, 53);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Back";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(327, 299);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form4";
             this.Text = "Form4";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
     }
 }
